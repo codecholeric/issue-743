cb9dd935ba923643c97d742370f23ea744dd6a3d
Peter Gafert <peter.gafert@tngtech.com>
2021-12-20 14:33:17 +0700

    add example to reproduce https://github.com/TNG/ArchUnit/issues/743

---
+++ src/test/java/SomeArchTest.java
@@ -0,0 +1,11 @@
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packages = "example")
+public class SomeArchTest {
+    @ArchTest
+    public static final ArchRule example = classes().should().bePublic();
+}
---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SomeTest {
+    @Test
+    void name() {
+        Assertions.assertEquals(1, 1);
+    }
+}
